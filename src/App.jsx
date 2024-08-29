f11d29c3b7c73f82d1f4605028b1b2052e72e320
xiena <129896535+zsxiena@users.noreply.github.com>
2024-08-29 13:27:33 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SearchForm from "./SearchForm";
+import ThemeToggle from "./ThemeToggle";
+import Gallery from "./Gallery";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
